b75764a373b7902084d19ecb9667badf4f174de0
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2025-09-06 15:16:41 -0300

    chore: add SERVER_PORT to environment configuration and update Vite server proxy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -2,37 +2,42 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
-export default defineConfig({
-  build: {
-    outDir: "./dist",
-    emptyOutDir: true,
-  },
-  plugins: [
-    tsconfigPaths({
-      projects: ["./tsconfig.app.json"],
-    }),
-    tanstackRouter({
-      target: "react",
-      autoCodeSplitting: true,
-      routesDirectory: "./src/lib/router/routes",
-      generatedRouteTree: "./src/lib/router/route-tree.gen.ts",
-      routeFileIgnorePattern: ".*\\.(test|spec)\\.(ts|tsx|js|jsx)$",
-    }),
-    tailwindcss(),
-    react(),
-  ],
-  resolve: {
-    alias: {
-      "@/web": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    build: {
+      outDir: "./dist",
+      emptyOutDir: true,
     },
-  },
-  server: {
-    proxy: {
-      "/api": "http://localhost:3030",
+    plugins: [
+      tsconfigPaths({
+        projects: ["./tsconfig.app.json"],
+      }),
+      tanstackRouter({
+        target: "react",
+        autoCodeSplitting: true,
+        routesDirectory: "./src/lib/router/routes",
+        generatedRouteTree: "./src/lib/router/route-tree.gen.ts",
+        routeFileIgnorePattern: ".*\\.(test|spec)\\.(ts|tsx|js|jsx)$",
+      }),
+      tailwindcss(),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        "@/web": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://localhost:${env.SERVER_PORT ?? 3030}`,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
 });
